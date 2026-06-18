78f42fc577f7534654b7033c45ec98d1a0e6dc2c
Shalom <shalom.obed2019@gmail.com>
2026-06-17 21:32:52 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    const sidebar = document.querySelector(".sidebar");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+
+    if(sidebar) {
+        sidebar.classList.toggle("open");
+    }
 }
